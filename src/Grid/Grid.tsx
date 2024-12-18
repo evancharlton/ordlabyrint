d91e7ce84153926afa87cbf3b064082f5a2e6eb3
Evan Charlton <evancharlton@gmail.com>
2024-12-18 19:11:07 +0100

    chore: Split out Grid

---
+++ src/Grid/Grid.tsx
@@ -0,0 +1,30 @@
+import { useGrid } from "../GridProvider";
+import { useGridSize } from "../GridSizeProvider";
+
+export const Grid = () => {
+  const { letters, addStep, path, allowedIds } = useGrid();
+  const { width, height } = useGridSize();
+
+  return (
+    <div>
+      <div>{path.join(" - ")}</div>
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: `repeat(${width}, 1fr)`,
+          gridTemplateRows: `repeat(${height}, 1fr)`,
+        }}
+      >
+        {letters.map((letter, i) => (
+          <button
+            key={`${letter}-${i}`}
+            disabled={!allowedIds[i]}
+            onClick={() => addStep(i)}
+          >
+            {letter}
+          </button>
+        ))}
+      </div>
+    </div>
+  );
+};
