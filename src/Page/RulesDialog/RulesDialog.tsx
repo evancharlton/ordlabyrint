05a41f5b309034b84228f8b4d58cf21f2aa7fea0
Evan Charlton <evancharlton@gmail.com>
2025-01-01 18:49:45 +0100

    Adjust buttons styling

--- src/Page/RulesDialog/RulesDialog.tsx
+++ src/Page/RulesDialog/RulesDialog.tsx
@@ -3,7 +3,7 @@ import { Modal } from "../Modal";
 export const RulesDialog = () => {
   return (
     <Modal title="Reglene" kind="rules">
-      <h3>Mål</h3>
+      <h3>Målet</h3>
       <p>
         Målet er å finne sekvenser av ord som starter på den ene kanten og
         berører til slutt motsatt side.
@@ -12,6 +12,14 @@ export const RulesDialog = () => {
         Hvis sekvensen begynner i ett hjørne, må den berøre det motsatte hjørne.
       </p>
       <p>Prøv å bruke så få ord som mulig!</p>
+      <hr />
+      <h3>Reglene</h3>
+      <p>Gyldige ord er:</p>
+      <ul>
+        <li>minst 3 bokstaver</li>
+        <li>kjent ord i ordlist</li>
+        <li>tegnet med opp/ned/venstre/høyre</li>
+      </ul>
     </Modal>
   );
 };
