1f4f0d51e1d6da621326443323f39770c92b5a91
Evan Charlton <evancharlton@gmail.com>
2025-01-11 16:06:59 +0100

    fix: Use outline for path
    Instead of drawing a second path to create the outline, tweak the
    scaling factors and use a css `outline` to accomplish it.

--- src/Grid/Snakes/Snakes.tsx
+++ src/Grid/Snakes/Snakes.tsx
@@ -2,11 +2,14 @@ import { useGridSize } from "../../GridSizeProvider";
 import { Solution, useHistory } from "../../HistoryProvider";
 import classes from "./Snakes.module.css";
 
+const MULTIPLIER = 100;
+const LINE_WIDTH = 10;
+
 const PreviousSolution = ({ path }: Solution) => {
   const coords = path
     .map((id, i) => {
       const [x, y] = id.split(",").map((v) => +v);
-      return `${i === 0 ? "M" : "L"} ${x * 10 + 5 - 1} ${y * 10 + 5 - 1}`;
+      return `${i === 0 ? "M" : "L"} ${x * MULTIPLIER + MULTIPLIER / 2 - LINE_WIDTH} ${y * MULTIPLIER + MULTIPLIER / 2 - LINE_WIDTH}`;
     })
     .join("\n");
 
@@ -14,14 +17,7 @@ const PreviousSolution = ({ path }: Solution) => {
     <>
       <path
         fill="transparent"
-        strokeWidth={2}
-        strokeLinejoin="round"
-        d={coords}
-        className={classes.border}
-      />
-      <path
-        fill="transparent"
-        strokeWidth={1}
+        strokeWidth={LINE_WIDTH}
         strokeLinejoin="round"
         d={coords}
         className={classes.snake}
@@ -42,7 +38,7 @@ export const Snakes = () => {
         width: "100%",
         height: "100%",
       }}
-      viewBox={`0 0 ${width * 10} ${height * 10}`}
+      viewBox={`0 0 ${width * MULTIPLIER} ${height * MULTIPLIER}`}
     >
       {previousSolutions.map((sol) => (
         <PreviousSolution key={sol.path.join("-")} {...sol} />
