c4231414be69fefd57a6af35677dfec9f41380d1
Evan Charlton <evancharlton@gmail.com>
2025-01-27 07:53:57 +0100

    feat: Make "new puzzle" button easier after reveal
    After the puzzle has been revealed, don't force the user to go back into
    the hamburger menu to get a new puzzle.
    
    I don't think I really like this, but .. whatever.

--- src/Buttons/Buttons.tsx
+++ src/Buttons/Buttons.tsx
@@ -1,18 +1,37 @@
+import { useNavigate, useParams } from "react-router";
 import { useGamePlay } from "../GameStateProvider";
 import { useSolution } from "../SolutionProvider";
 import classes from "./Buttons.module.css";
-import { MdBackspace, MdKeyboardReturn, MdRestartAlt } from "react-icons/md";
+import {
+  MdBackspace,
+  MdKeyboardReturn,
+  MdOutlineAutorenew,
+  MdRestartAlt,
+} from "react-icons/md";
 
 export const Buttons = () => {
+  const navigate = useNavigate();
+  const { lang, size } = useParams();
   const { state } = useSolution();
   const { backspace, reset, addWord, current, path, solved } = useGamePlay();
 
   return (
     <div className={classes.container}>
       <div className={classes.buttons}>
-        <button disabled={state !== "pending"} onClick={() => reset()}>
-          <MdRestartAlt />
-        </button>
+        {state === "solved" ? (
+          <button
+            title="Nytt puslespill"
+            onClick={() => {
+              navigate(`/${lang}/${size}/${Date.now()}`);
+            }}
+          >
+            <MdOutlineAutorenew />
+          </button>
+        ) : (
+          <button disabled={state !== "pending"} onClick={() => reset()}>
+            <MdRestartAlt />
+          </button>
+        )}
         <button
           disabled={solved || path.length === 0}
           onClick={() => backspace()}
